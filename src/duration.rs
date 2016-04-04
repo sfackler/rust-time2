6a9daaa04a01d6d2b548d7ef21e294269f75d48e
Steven Fackler <sfackler@gmail.com>
2016-04-03 21:01:15 -0700

    Drop muldiv dependency
    It's LGPL :(

--- src/duration.rs
+++ src/duration.rs
@@ -1,5 +1,4 @@
 use std::time::Duration;
-use muldiv::MulDiv;
 
 const NANOS_PER_SEC: u64 = 1_000_000_000;
 const MILLIS_PER_SEC: u64 = 1_000;
@@ -51,12 +50,78 @@ impl DurationExt for Duration {
     fn div_u64(&self, rhs: u64) -> Duration {
         let secs = self.as_secs() / rhs;
         let carry = self.as_secs() - secs * rhs;
-        let extra_nanos = carry.mul_div_floor(NANOS_PER_SEC, rhs)
-                               .expect("overflow when dividing duration");
+        let extra_nanos = mul_div(carry, NANOS_PER_SEC, rhs)
+                              .expect("overflow when dividing duration");
         let nanos = (self.subsec_nanos() as u64 / rhs + extra_nanos) as u32;
         debug_assert!(nanos < NANOS_PER_SEC as u32);
         Duration::new(secs, nanos)
     }
+}
+
+fn mul_div(a: u64, b: u64, c: u64) -> Option<u64> {
+    let m0 = a.wrapping_mul(b);
+    let m1 = mul_u64_hi(a, b);
+    div_u128(m1, m0, c)
+}
+
+// hacker's delight 2nd ed 8-2
+fn mul_u64_hi(u: u64, v: u64) -> u64 {
+    let u0 = u & 0xFFFF_FFFF;
+    let u1 = u >> 32;
+    let v0 = v & 0xFFFF_FFFF;
+    let v1 = v >> 32;
+    let w0 = u0 * v0;
+    let t = u1 * v0 + (w0 >> 32);
+    let w1 = t & 0xFFFF_FFFF;
+    let w2 = t >> 32;
+    let w1 = u0 * v1 + w1;
+    u1 * v1 + w2 + (w1 >> 32)
+}
+
+//hacker's delight 2nd ed 9-3
+fn div_u128(u1: u64, u0: u64, v: u64) -> Option<u64> {
+    let b = 0x1_0000_0000;
+
+    if u1 >= v {
+        return None;
+    }
+
+    let s = v.leading_zeros();
+    let v = v << s;
+    let vn0 = v & 0xFFFF_FFFF;
+    let vn1 = v >> 32;
+
+    let un32 = (u1 << s) | (u0.wrapping_shr(64 - s)) & (-(s as i64) >> 63) as u64;
+    let un10 = u0 << s;
+
+    let un1 = un10 >> 32;
+    let un0 = un10 & 0xFFFF_FFFF;
+
+    let mut q1 = un32 / vn1;
+    let mut rhat = un32 - q1 * vn1;
+
+    while q1 >= b || q1 * vn0 > b * rhat + un1 {
+        q1 -= 1;
+        rhat += vn1;
+        if rhat >= b {
+            break;
+        }
+    }
+
+    let un21 = un32 * b + un1 - q1 * v;
+
+    let mut q0 = un21 / vn1;
+    rhat = un21 - q0 * vn1;
+
+    while q0 >= b || q0 * vn0 > b * rhat + un0 {
+        q0 -= 1;
+        rhat += vn1;
+        if rhat >= b {
+            break;
+        }
+    }
+
+    Some(q1 * b + q0)
 }
 
 #[cfg(test)]
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,6 @@
 //! Experimental extensions to the `std::time` module.
 #![warn(missing_docs)]
 
-extern crate muldiv;
-
 pub use duration::DurationExt;
 #[cfg(feature = "beta")]
 pub use system_time::SystemTimeExt;
