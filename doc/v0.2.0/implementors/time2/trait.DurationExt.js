663ea1ec1205ab92a74bdabcad9990ca9964d7f6
Steven Fackler <sfackler@gmail.com>
2016-04-04 21:31:51 -0700

    Release v0.2.0

---
+++ doc/v0.2.0/implementors/time2/trait.DurationExt.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['time2'] = [];implementors['time2'] = [];implementors['time2'] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ doc/v0.2.0/time2/system_time/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"trait":[["SystemTimeExt","Extensions to the `SystemTime` type."]]});
